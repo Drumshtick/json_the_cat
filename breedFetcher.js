32f07095d8afb08f0757157f46070cb16baabe08
Drumshtick <ekodnar@outlook.com>
2021-11-04 19:37:20 +0000

    Made request code, with error handling for no breed found and failed request

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,14 @@
 const request = require('request');
+let breed = process.argv.splice(2,1);
+
+request(`https://api.thecatapis.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error !== null) {
+    console.log(error);
+    process.exit();
+  } else if (body.length === 2) {
+    console.log(`Cat breed '${breed}' not found.`);
+    process.exit();
+  } else {
+    console.log(JSON.parse(body)[0].description);
+  }
+});
